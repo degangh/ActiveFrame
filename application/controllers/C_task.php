8d88c0e95cd5a08825cce0a93ddf8e6154bbc69a
degangh <degang.huang@gmail.com>
2018-06-14 13:55:12 +0930

    add sample app

---
+++ application/controllers/C_task.php
@@ -0,0 +1,27 @@
+<?php
+class C_task extends CController
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
+	public function index()
+	{
+		/*--
+		Load your lib or model to process reuqest
+		--*/
+		loadview('task/index',$data);
+    }
+    
+    public function save()
+    {
+        if (!$_POST) throw new Exception ("no post");
+
+        else
+        {
+            var_dump($_POST);
+        }
+    }
+}
+?>
